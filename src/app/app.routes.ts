011c5e07a08bc525c31b18a45819de844075d106
Sebastian Wolff <bastiwolff432@gmail.com>
2024-06-30 11:09:26 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'registration', component: RegistrationComponent},
+];

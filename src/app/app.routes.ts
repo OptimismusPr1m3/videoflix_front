7d566d41a5482ffbd45aa969de833614b9c6fadf
bastiwolff432 <bastiwolff432@gmail.com>
2024-07-08 18:33:10 +0000

    added landingpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegistrationComponent } from './authentication/registration/registration.component';
+import { LandingpageComponent } from './authentication/landingpage/landingpage.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent},
+    { path: '', component: LandingpageComponent},
+    { path: 'login', component: LoginComponent},
     { path: 'registration', component: RegistrationComponent},
 ];

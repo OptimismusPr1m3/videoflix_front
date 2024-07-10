6b7d6c7eb90cb0e434ba2a0835bf4690bf9e1456
bastiwolff432 <bastiwolff432@gmail.com>
2024-07-10 14:40:34 +0000

    added backend communication service + some styling for landing page

--- src/app/authentication/landingpage/landingpage.component.ts
+++ src/app/authentication/landingpage/landingpage.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { BackendCommunicationService } from '../../services/backend-communication.service';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, MatIconModule, ReactiveFormsModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
+
+  mail = new FormControl('');
+
+  constructor(private http: BackendCommunicationService) {
+    //this.http.getMails().subscribe(data => console.log(data))
+  }
+
+  checkMail() {
+    this.http.getMails().subscribe(data => console.log(data))
+    /*if (this.mail.valid) {
+      console.log(this.mail.value)  
+    } else {
+      console.log('invalid')
+    }
+    */
+  }
 
 }

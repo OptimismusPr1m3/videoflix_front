db90c6896f92293b861f1e87f11bc4edbec1548a
Sebastians Macbook Pro <bastiwolff432@gmail.com>
2024-08-29 13:04:21 +0200

    continued on videoplayer controls

--- src/app/main-content/videoplayer/videoplayer.component.ts
+++ src/app/main-content/videoplayer/videoplayer.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GlobalVariablesService } from '../../services/global-variables.service';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -11,10 +11,30 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './videoplayer.component.scss'
 })
 export class VideoplayerComponent {
+
+  @ViewChild('videoFrame') videoFrame!: ElementRef<HTMLVideoElement>
+  @ViewChild('videoWrapper') videoWrapper!: ElementRef<HTMLDivElement>
+
   constructor(public globals: GlobalVariablesService){}
 
   closePlayer() {
     this.globals.isVidOpen.set(!this.globals.isVidOpen())
   }
+
+  openFullscreen(){
+    const videoWrapper = this.videoWrapper.nativeElement
+    videoWrapper.requestFullscreen()
+    this.prepareVideo()
+  }
+
+  prepareVideo(){
+    const videoFrame = this.videoFrame.nativeElement
+    videoFrame.pause()
+    videoFrame.currentTime = 0
+    videoFrame.play()
+    videoFrame.style.height = '100%'
+    videoFrame.style.width = '100%'
+    videoFrame.muted = false
+  } 
 
 }

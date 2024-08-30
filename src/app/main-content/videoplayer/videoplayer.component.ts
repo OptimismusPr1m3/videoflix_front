aba053e6b5dff73947dc79231cf7baf2b0dec84c
Sebastians Macbook Pro <bastiwolff432@gmail.com>
2024-08-30 11:21:00 +0200

    continued custom controls for videoplayer

--- src/app/main-content/videoplayer/videoplayer.component.ts
+++ src/app/main-content/videoplayer/videoplayer.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GlobalVariablesService } from '../../services/global-variables.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-videoplayer',
@@ -14,6 +16,12 @@ export class VideoplayerComponent {
 
   @ViewChild('videoFrame') videoFrame!: ElementRef<HTMLVideoElement>
   @ViewChild('videoWrapper') videoWrapper!: ElementRef<HTMLDivElement>
+
+  videoDuration: number = 0
+  currentTime: number = 0
+  isVidFullScreen: boolean = false
+  isPlaying: boolean = false
+  materialButtonString: string = 'pause'
 
   constructor(public globals: GlobalVariablesService){}
 
@@ -32,9 +40,40 @@ export class VideoplayerComponent {
     videoFrame.pause()
     videoFrame.currentTime = 0
     videoFrame.play()
+    this.isPlaying = true
     videoFrame.style.height = '100%'
     videoFrame.style.width = '100%'
     videoFrame.muted = false
+    this.isVidFullScreen = true
+    //console.log(videoFrame.duration)
   } 
+
+  seek(event: any) {
+    const video = this.videoFrame.nativeElement;
+    video.currentTime = event.target.value;
+  }
+
+  updateProgressBar() {
+    const video = this.videoFrame.nativeElement;
+    this.currentTime = video.currentTime;
+  }
+
+  initializeVideo() {
+    const video = this.videoFrame.nativeElement;
+    this.videoDuration = video.duration
+  }
+
+  toggleVideo() {
+    const videoFrame = this.videoFrame.nativeElement
+    if (this.isPlaying) {
+      videoFrame.pause()
+      this.materialButtonString = 'play_arrow'
+      this.isPlaying = false
+    } else {
+      videoFrame.play()
+      this.isPlaying = true
+      this.materialButtonString = 'pause'
+    }
+  }
 
 }

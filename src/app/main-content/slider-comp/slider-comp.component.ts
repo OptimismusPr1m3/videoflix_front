324ea53a4698cc7c1fc60ed4066f334404c05ca8
Sebastian Wolff <bastiwolff432@gmail.com>
2024-11-02 22:36:02 +0100

    added spinner for main comp

--- src/app/main-content/slider-comp/slider-comp.component.ts
+++ src/app/main-content/slider-comp/slider-comp.component.ts
@@ -10,11 +10,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { VideoItemComponent } from './video-item/video-item.component';
 import { BackendCommunicationService } from '../../services/backend-communication.service';
 import { VideoItem } from '../../models/videoItem.class';
+import { NgxSpinnerComponent, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-slider-comp',
   standalone: true,
-  imports: [CommonModule, MatIconModule, VideoItemComponent],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    VideoItemComponent,
+    NgxSpinnerComponent,
+  ],
   templateUrl: './slider-comp.component.html',
   styleUrl: './slider-comp.component.scss',
 })
@@ -37,59 +43,67 @@ export class SliderCompComponent {
   groupedSliderVids: { [key: string]: VideoItem[] } = {};
   numberOfPacks: string[] = [];
 
-  videoItemCardsAmount: number = 4 // amount of videocarditems in a row which should be displayed 
-
+  videoItemCardsAmount: number = 4; // amount of videocarditems in a row which should be displayed
 
   windowSize: number = 0;
 
-  constructor(private backService: BackendCommunicationService) {}
+  constructor(
+    private backService: BackendCommunicationService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
-    this.resizeCategory('category1', 1, 1)
-    this.resizeCategory('category2', 2, 2)
+    this.resizeCategory('category1', 1, 1);
+    this.resizeCategory('category2', 2, 2);
     // console.log('posi 1 : ',this.currentPosition1)
     // console.log('posi 2 : ',this.currentPosition2)
   }
 
-  resizeCategory(category: string, numbX: number, currentIndex: number) { //numb is the currentPosition 1 or 2 
+  resizeCategory(category: string, numbX: number, currentIndex: number) {
+    //numb is the currentPosition 1 or 2
     const sliderCategory = this.getRightCategory(category);
     const itemWidth = sliderCategory.offsetWidth;
-    const cIndex = this.getIndex(currentIndex)
+    const cIndex = this.getIndex(currentIndex);
     const cPosition = -cIndex * itemWidth; // sync position with currentslide index
     sliderCategory.style.transform = `translateX(${cPosition}px)`;
-    this.setXPosition(numbX, cPosition) //sets the new x-Position for the slide comp which has been slided to right or left
+    this.setXPosition(numbX, cPosition); //sets the new x-Position for the slide comp which has been slided to right or left
   }
 
   setXPosition(numbX: number, newcPosition: number) {
     if (numbX == 1) {
-      this.currentPosition1 = newcPosition
-    } else if (numbX == 2){
-      this.currentPosition2 = newcPosition
+      this.currentPosition1 = newcPosition;
+    } else if (numbX == 2) {
+      this.currentPosition2 = newcPosition;
     }
   }
 
-  getIndex(cIndex: number){
+  getIndex(cIndex: number) {
     if (cIndex == 1) {
-      return this.currentIndex1
+      return this.currentIndex1;
     } else {
-      return this.currentIndex2
+      return this.currentIndex2;
     }
   }
 
   ngOnInit() {
-    this.windowSize = window.innerWidth
-    console.log(this.windowSize)
+    this.windowSize = window.innerWidth;
+    console.log(this.windowSize);
+    this.spinner.show();
     this.backService.fetchVideoItems().subscribe({
       next: (resp) => {
-        console.log('Hier die Videos: ', resp)
+        
+        console.log('Hier die Videos: ', resp);
         this.groupVideoItems(resp);
       },
       error: (err) => {
         console.error(err);
+        this.spinner.hide();
       },
       complete: () => {
-        console.log('hier die groupedslidervids: ',this.groupedSliderVids), console.log('Hier die numberofPacks: ',this.numberOfPacks);
+        console.log('hier die groupedslidervids: ', this.groupedSliderVids);
+        console.log('Hier die numberofPacks: ', this.numberOfPacks);
+        this.spinner.hide()
       },
     });
   }
@@ -99,7 +113,10 @@ export class SliderCompComponent {
     let tempPack: VideoItem[] = [];
     resp.forEach((video: VideoItem, index: number) => {
       tempPack.push(new VideoItem(video));
-      if (tempPack.length === this.videoItemCardsAmount || index === resp.length - 1) {
+      if (
+        tempPack.length === this.videoItemCardsAmount ||
+        index === resp.length - 1
+      ) {
         this.groupedSliderVids[`group${groupIndex}`] = [...tempPack];
         tempPack = [];
         groupIndex++;
@@ -109,23 +126,29 @@ export class SliderCompComponent {
   }
 
   nextVideos(direction: string, category: string, numbX: number) {
-    const sliderCategory = this.getRightCategory(category)
-    const itemWidth = sliderCategory.offsetWidth
-    this.setCategoryIndex(category, direction)
+    const sliderCategory = this.getRightCategory(category);
+    const itemWidth = sliderCategory.offsetWidth;
+    this.setCategoryIndex(category, direction);
     const cPosition = -this.getIndex(numbX) * itemWidth; // Berechne neue Position basierend auf dem Index
     sliderCategory.style.transform = `translateX(${cPosition}px)`;
-  }  
+  }
 
   setCategoryIndex(category: string, direction: string) {
     if (category === 'category1') {
       if (direction === '-') {
-        this.currentIndex1 = Math.min(this.currentIndex1 + 1, this.numberOfPacks.length - 1); // Grenze oben setzen
+        this.currentIndex1 = Math.min(
+          this.currentIndex1 + 1,
+          this.numberOfPacks.length - 1
+        ); // Grenze oben setzen
       } else {
         this.currentIndex1 = Math.max(this.currentIndex1 - 1, 0); // Grenze unten setzen
       }
     } else if (category === 'category2') {
       if (direction === '-') {
-        this.currentIndex2 = Math.min(this.currentIndex2 + 1, this.numberOfPacks.length - 1); // Grenze oben setzen
+        this.currentIndex2 = Math.min(
+          this.currentIndex2 + 1,
+          this.numberOfPacks.length - 1
+        ); // Grenze oben setzen
       } else {
         this.currentIndex2 = Math.max(this.currentIndex2 - 1, 0); // Grenze unten setzen
       }
@@ -135,7 +158,7 @@ export class SliderCompComponent {
   getRightCategory(category: string) {
     switch (category) {
       case 'category1':
-        return this.category1.nativeElement;          
+        return this.category1.nativeElement;
       case 'category2':
         return this.category2.nativeElement;
       default:
